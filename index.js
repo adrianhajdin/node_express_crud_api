f901f35934c4c8191d7ab1045ff9e27356a8f403
adrianhajdin <adrianhajdin00@gmail.com>
2020-07-17 09:47:33 +0200

    git ignore

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.use(bodyParser.json());
 
 app.use("/people", usersRoutes);
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
-app.all("*", (req, res) =>
-  res.send("You've tried reaching a route that doesn't exist.")
-);
+app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
 
-app.listen(PORT, () =>
-  console.log(`Server running on port: http://localhost:${PORT}`)
-);
+app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
